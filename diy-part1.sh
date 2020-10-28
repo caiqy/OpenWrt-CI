2928a5b51c615a5eeeaf0c9feba2b3d535e6d39e
Caiqy <caiqy94@gmail.com>
2020-10-28 15:37:47 +0800

    更新包

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,8 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/argon
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/jddailybonus
+git clone https://github.com/tty228/luci-app-serverchan package/serverchan
+
--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,41 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+# rm unused package
+rm feeds/kenzo/luci-app-jd-dailybonus/ -rf
+rm feeds/kenzo/luci-app-advancedsetting -rf
+rm feeds/kenzo/luci-app-clash -rf
+rm feeds/kenzo/luci-app-eqos -rf
+rm feeds/kenzo/luci-app-gost -rf
+rm feeds/kenzo/gost -rf
+rm feeds/kenzo/luci-app-openclash -rf
+rm feeds/kenzo/luci-app-ssr-plus -rf
+rm feeds/kenzo/luci-theme-argon_new -rf
+rm feeds/kenzo/luci-theme-atmaterial -rf
+rm feeds/kenzo/luci-theme-edge -rf
+rm feeds/kenzo/luci-theme-ifit -rf
+rm feeds/kenzo/luci-theme-opentomato -rf
+rm feeds/kenzo/luci-theme-opentomcat -rf
+rm feeds/kenzo/screenshot -rf
+
+rm feeds/small/brook -rf
+rm feeds/small/shadowsocksr-libev -rf
+rm feeds/small/simple-obfs -rf
+rm feeds/small/trojan -rf
+rm feeds/small/trojan-go -rf
+rm feeds/small/trojan-plus -rf
+
+rm package/lean/UnblockNeteaseMusic -rf
+rm package/lean/UnblockNeteaseMusicGo -rf
+rm package/lean/luci-app-unblockmusic -rf
+rm package/lean/luci-app-filetransfer -rf
+
+
+# reload feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.9/g' package/base-files/files/bin/config_generate
 sed -i '/set network.$1.netmask/a\\t\t\t\tset network.$1.dns='"'192.168.2.1'" package/base-files/files/bin/config_generate
